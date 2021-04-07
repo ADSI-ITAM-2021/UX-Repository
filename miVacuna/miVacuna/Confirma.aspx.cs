e166d5a8877300794aabdba9f12e0cfab4c59554
YOSSHUA <yocisneros@outlook.com>
2021-04-07 15:43:03 -0500

    Listo

---
+++ miVacuna/miVacuna/Confirma.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace miVacuna
+{
+    public partial class Confirma : System.Web.UI.Page
+    {
+        private string curp, correo;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["curp"] != null && Session["correo"] != null)
+            {
+                txtCurp.Text = Session["curp"].ToString();
+                txtCorreo.Text = Session["correo"].ToString();
+            }
+            else
+            {
+                Response.Redirect("Index.aspx");
+            }
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Index.aspx");
+        }
+
+        protected void btnReg_Click(object sender, EventArgs e)
+        {
+            Session["nom"] = txtNom.Text;
+            Response.Redirect("Elegir.aspx");
+        }
+    }
+}
--- miVacuna/miVacuna/index.aspx.cs
+++ miVacuna/miVacuna/index.aspx.cs
@@ -7,11 +7,16 @@ using System.Web.UI.WebControls;
 
 namespace miVacuna
 {
-    public partial class index : System.Web.UI.Page
+    public partial class O4 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnReg_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Registro.aspx");
         }
     }
 }
